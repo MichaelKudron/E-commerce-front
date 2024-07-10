670b29becd9738b11b9784e1ca859d9ca56aa4f8
MichaelKudron <kobrys87@gmail.com>
2024-07-10 16:01:56 +0200

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { StartPageComponent } from './components/start-page/start-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        title:"start",
+        component:StartPageComponent
+    },
+    {
+        path:'login',
+        title:'login',
+        component:LoginComponent
+    },
+    {
+        path:'register',
+        title:'register',
+        component:RegisterComponent
+    }
+];
